6b4e7488231c03011dc8302190f915ebac2dae9f
Jared Botte <jaredbotte@gmail.com>
2021-06-04 09:26:48 -0400

    Changed socket to UDP and put into seperate thread. Also decoded messages into strings

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 import socket
+import time
+
 from aircraft import Aircraft
+from multiprocessing import Process, Manager
 #from guiapp import AircraftVisualizer  # May change this to * later if needed
 
 HOST = '192.168.0.47'  # Local IP address of the PiAware
 PORT = 30003  # Basestation data port
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def setup_piaware_data_stream():
-    return socket.create_connection((HOST, PORT))
+def socket_listening_thread(visible_aircraft):
+    while True:
+        try:
+            messages = sock.recvfrom(4096)[0].decode("utf-8").split('\n')
+            for message in messages:
+                parse_message(message, visible_aircraft)
+        except socket.error as e:
+            if str(e) == "[Errno 35] Resource temporarily unavaliable":
+                continue
+            else:
+                print(str(e))
+                sock.close()
+
+
+def setup_piaware_data_stream(visible_aircraft):
+    sock.connect((HOST, PORT))
+    # TODO: Wait for connection (ignore Errno 36?). Look at non blocking socket issues.
+    return Process(target=socket_listening_thread, args=(visible_aircraft,))
 
 
 def parse_msg(msg_arr, visible_aircraft):
-    print(msg_arr[4])
     if msg_arr[4] in visible_aircraft:
         visible_aircraft[msg_arr[4]].update(msg_arr)
     else:
@@ -21,26 +40,27 @@ def parse_msg(msg_arr, visible_aircraft):
 
 
 def parse_message(message, visible_aircraft):
-    message_arr = message.split(b',')
-    if message_arr[0] == b'MSG':
+    message_arr = message.split(',')
+    if message_arr[0] == "MSG":
         parse_msg(message_arr, visible_aircraft)
     else:
         pass
 
 
-if __name__ == '__main__':
-    datasocket = setup_piaware_data_stream()
-    datasocket.setblocking(False)
-    visible_aircraft = {}
-    #AircraftVisualizer().run()
+def main():
+    man = Manager()
+    visible_aircraft = man.dict({})
+    data_stream_thread = setup_piaware_data_stream(visible_aircraft)
+    data_stream_thread.start()
+
+    # AircraftVisualizer().run()
+    # TODO: Create Cleanup Thread to remove old aircraft
+    # TODO: Create GUI Thread
+    # TODO: Determine how to kill the program and create cleanup function
     while True:
-        try:
-            for data in datasocket.recv(4096).split(b'\n'):
-                parse_message(data, visible_aircraft)
-                print(len(visible_aircraft))
-                # Clean old aircraft
-        except socket.error as e:
-            if str(e) == "[Errno 35] Resource temporarily unavailable":
-                continue
-            else:
-                datasocket.close()
+        print(f"Current Visible Aircraft: {len(visible_aircraft)}", end='\r')
+        time.sleep(0.5)  # This is needed to prevent pycharm from being able to end the program
+
+
+if __name__ == '__main__':
+    main()
